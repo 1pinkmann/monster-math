26f16ff330b4718182cc548c0a83192e94a22aaa
Pinkmann <korgov.vova@icloud.com>
2021-04-14 23:38:51 +0300

    final

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -23,7 +23,7 @@ const FormContainer = () => {
   // new https://script.google.com/macros/s/AKfycbwJ9H88mHG6x2w8IGDOZocYyZCciZ4SUDmC-SfbuDjgCILgeQNsqpw7L9rqldSjLEJskg/exec
   // https://script.google.com/macros/s/AKfycbwPfO3SFDbsqwZ0mFuhUvlIdZZh37USZ0abhxeprgs0UKwKOAC3O7kpBEteZFBq6zAKCQ/exec
   const sendToGoggleSheets = async (stringParams) => {
-    const googleSheetsUrl = 'https://script.google.com/macros/s/AKfycbwJ9H88mHG6x2w8IGDOZocYyZCciZ4SUDmC-SfbuDjgCILgeQNsqpw7L9rqldSjLEJskg/exec'
+    const googleSheetsUrl = 'https://script.google.com/macros/s/AKfycbxOKehYmywe9YTiFBNFonjQXXMThPMeSXiwhdkctHmnWfgmgHbdQ-RIWUKqgawwrTGS-g/exec'
 
     try {
       await fetch(`${googleSheetsUrl}?${stringParams}`, {
